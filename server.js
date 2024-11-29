2f0b41d6c2e1a56512ec1680dd80c99c8c26595d
Tanmaydeep-Singh <92568870+Tanmaydeep-Singh@users.noreply.github.com>
2024-11-29 10:32:13 +0530

    dev: Structure update

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
-const app = require("./app");
-const connectDB = require("./config/db");
+const app = require("./src/app");
+const connectDB = require("./src/config/db");
 require("dotenv").config();
 
 connectDB();
 
-// Start the server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
